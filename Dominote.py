a12abc0fe3a7d2d83a98233a64fc5d7136629ceb
Ruchir <email.ruchir@gmail.com>
2020-02-03 21:35:24 +0530

    Dominote
    Finding dominant note from local midicsv program output (.csv) file

---
+++ Dominote.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import array
+pd.set_option('display.max_rows', 200)
+
+# Reading midicsv output file to dataframe
+df = pd.read_csv('RagaJhinjhoti22.csv')  
+print(df)  # printing the data frame
+type(df)
+df.shape
+df.info()
+df.columns = ['col1', 'col2', 'NoteStatus', 'col4', 'NoteNr', 'NoteValo']
+print(df)
+type(df)
+df.shape
+df.info()
+col1 = df['col1']
+col2 = df['col2']
+NoteStatus = df['NoteStatus']
+col4 = df['col4']
+NoteNr = df['NoteNr']
+NoteValo = df['NoteValo']
+pd.set_option('display.max_rows', 200)
+gd = df.groupby(NoteNr).size()
+print(gd)
+df = gd
+Max = max(df)
+print(Max)
+Dominote = df.idxmax()
+print(Dominote)
+Note_Number = (int)((Dominote % 12))
+Scale = (int)(Dominote/12)
+print(Note_Number)
+df.item = (['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'])
+EndP = df.item[Note_Number]
+EndQ = (str)(Scale-1)
+print(EndP+"("+EndQ+")")
