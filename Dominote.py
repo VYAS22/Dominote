3ddf7c061670704c1cf281018cd7c2f5697d945c
Ruchir <email.ruchir@gmail.com>
2020-02-04 09:42:54 +0530

    updated

--- Dominote.py
+++ Dominote.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 import array
 pd.set_option('display.max_rows', 200)
-
-# Reading midicsv output file to dataframe
-df = pd.read_csv('RagaJhinjhoti22.csv')  
+df = pd.read_csv('data.dat')  # Reading midicsv output file to dataframe
+df1 = df.iloc[:, :5]
+df2 = df.iloc[:, 5:]
 print(df)  # printing the data frame
 type(df)
 df.shape
 df.info()
+df.drop(5)
 df.columns = ['col1', 'col2', 'NoteStatus', 'col4', 'NoteNr', 'NoteValo']
 print(df)
 type(df)
