b575deb7b9dbd04cdc90df274d82701d7cf37473
Ruchir <ruchir.vyas@gmail.com>
2024-07-19 13:34:53 +0530

    Create CQT.py
    Constant Q Transform

---
+++ CQT.py
@@ -0,0 +1,20 @@
+
+import librosa
+import numpy as np
+
+#Load audio file
+y, sr = librosa.load("your_audio_file.wav")
+
+#Compute CQT
+C = librosa.cqt(y, sr=sr)
+
+#Visualize the CQT
+import matplotlib.pyplot as plt
+import librosa.display
+
+librosa.display.specshow(librosa.amplitude_to_db(np.abs(C), ref=np.max),
+sr=sr, x_axis='time', y_axis='cqt_note')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Constant-Q power spectrum')
+plt.tight_layout()
+plt.show()
