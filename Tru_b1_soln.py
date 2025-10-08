5b23cece60f1d740b36d37d38d1efca2d4dd215e
Ruchir <email.ruchir@gmail.com>
2025-10-08 17:31:49 +0530

    Updated code files

---
+++ Tru_b1_soln.py
@@ -0,0 +1,37 @@
+# Cumulative time for each note
+time_on = [0] * 128
+Notebox = [0] * 13
+
+# Last 'on' time for each note. -1 means that the note isn't on
+last_on = [-1] * 128
+
+
+def map_line(line):
+    # turn a line from the data file into a tuple
+    (_, timestamp, status, _, note, _) = line.split(',')
+    return (
+        int(timestamp),
+        status.strip() == 'Note_on_c',
+        int(note)
+        )
+
+
+data_file = open('data.dat', 'r')
+data = [map_line(line) for line in data_file.readlines()]
+data_file.close()
+
+data.sort()
+
+for (time, note_on, note) in data:
+    if note_on and last_on[note] == -1:
+        last_on[note] = time
+    elif not note_on and last_on[note] != -1:
+        time_on[note] += time - last_on[note]
+        last_on[note] = -1
+
+for i in range(128):
+        j = i%12    
+        Notebox[j] += time_on[i]
+
+for j in range(12): 
+        print('{}, {}'.format(j, Notebox[j]))
